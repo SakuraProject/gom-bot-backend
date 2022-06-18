8cb4047b81ef32e25fcd8a06746f2907f47e53d3
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-06-18 09:31:42 +0000

    基礎ファイル作成

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    }
+);
