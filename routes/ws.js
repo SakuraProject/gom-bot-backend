4d85f01a44a7454788f5cc26c0adaf4fee0d45d5
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-07-13 16:52:31 +0900

    fix: 通信方法を改善

--- routes/ws.js
+++ routes/ws.js
@@ -3,25 +3,35 @@ const expressWs = require('express-ws')
 const router = express.Router()
 expressWs(router);
 
-router.ws('/', (ws, req) => {
-  console.log('Websocket is connected');
-  ws.on('message', msg => {
-    if(msg=="numdata"){
-      ws.send(""+module.exports.wait.length)
-      while(module.exports.wait.length!=0){
-         ws.send(module.exports.wait.shift())
-      }
-    }else{
+router.ws('/', async (ws, req) => {
+  console.log('Ú‘±¬Œ÷');
+  ws.on('message',async msg => {
     data = JSON.parse(msg);
-    if(!module.exports.res[data["cmd"]]){
-      module.exports.res[data["cmd"]] = [];
-    }
-    module.exports.res[data["cmd"]][data["args"]["id"]] = data["args"];
+    if(data["type"]=="cmd"){
+      data["args"] = await module.exports[data["cmd"]](data["args"]);
+      data["type"] = "res";
+      ws.send(JSON.stringify(data));
+    }else if(data["type"]=="res"){
+      if(!module.exports.res[data["cmd"]]){
+        module.exports.res[data["cmd"]] = [];
+      }
+      module.exports.res[data["cmd"]][data["args"]["id"]] = data["args"];
     }
   })
 })
 obj = {}
 obj.router = router;
 module.exports = obj;
-module.exports.wait = [];
 module.exports.res = [];
+module.exports.jsk = async function(args){
+  const res = new Promise((resolve) => resolve(eval(args["code"])));
+  args["res"] = res.then(async (out) => {
+    if (typeof out !== "string") {
+      out = require("util").inspect(out, { depth: 0 });
+    }
+    return out
+  }).catch(async (err) => {
+    return err.toString();
+  });
+  return args
+}
