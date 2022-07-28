f875a0481613ebcda062046d904ee601fd2b0f2f
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-07-28 21:19:51 +0900

    rocationsを読み込み

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const home = require('./routes/home');
 const ws = require('./routes/ws');
+const rocations = require('./routes/rocations');
 
 app.use('/', home);
 app.use('/ws',ws.router);
+app.use('/rocations',rocations);
 app.listen(3000, () => {
     console.log('Server started on port 3000');
     }
