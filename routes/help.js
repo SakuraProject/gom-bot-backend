228df7e7dd26fb06b7d52feb02e52c2eb84840d0
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-08-07 21:08:58 +0900

    fix: バグ修正

--- routes/help.js
+++ routes/help.js
@@ -5,7 +5,7 @@ lpack = [];
 lpack["ja"] = {"catdesc": "このページはSakuraBotのコマンドヘルプです。カテゴリを選択することにより、カテゴリに含まれるコマンドを見ることが出来ます","cmddesc":"このページはSakuraBotのコマンドヘルプです。コマンドを選択するとコマンドの説明を表示します。もしグループコマンドの場合はグループ内のコマンドリストを表示します"};
 lpack["en"] = {"catdesc": "This page is the command help for SakuraBot. By selecting a category, you can see the commands included in that category.", "cmddesc": "This page is the command help for SakuraBot. By selecting a command, you can see a description of the command. If it is a group command, a list of commands in the group will be displayed"};
 router.get('/', async function (req, res) {
-    const ws = module.parent.exports.ws;
+    const ws = module.parent.exports.ws.exports.ws;
     const { l,cat,cmd } = req.query;
     if (!l){
         ll = "ja";
@@ -18,9 +18,9 @@ router.get('/', async function (req, res) {
         r["cmd"] = "command";
         r["args"] = [];
         r["args"]["id"] = cmd;
-        ws.exports.ws.send(JSON.stringify(r));
+        ws.send(JSON.stringify(r));
         sleep.sleep(1);
-        wsr = ws.exports.res["command"][cmd];
+        wsr = module.parent.exports.ws.exports.res["command"][cmd];
     }else if(cat){
         r = [];
         r["type"] = "cmd";
@@ -28,9 +28,9 @@ router.get('/', async function (req, res) {
         r["args"] = [];
         r["args"]["id"] = cat;
         r["args"]["l"] = ll;
-        ws.exports.ws.send(JSON.stringify(r));
+        ws.send(JSON.stringify(r));
         sleep.sleep(1);
-        wsr = ws.exports.res["help_cmdlist"][cmd];
+        wsr = module.parent.exports.ws.exports.res["help_cmdlist"][cmd];
     }else{
         r = [];
         r["type"] = "cmd";
@@ -38,9 +38,9 @@ router.get('/', async function (req, res) {
         r["args"] = [];
         r["args"]["id"] = "catlist";
         r["args"]["l"] = ll;
-        ws.exports.ws.send(JSON.stringify(r));
+        ws.send(JSON.stringify(r));
         sleep.sleep(1);
-        wsr = ws.exports.res["help_catlist"]["catlist"];
+        wsr = module.parent.exports.ws.exports.res["help_catlist"]["catlist"];
     }
     res.render('help',{wsr: wsr,cmd: cmd,cat: cat,l: ll,lpack: lpack});
         
