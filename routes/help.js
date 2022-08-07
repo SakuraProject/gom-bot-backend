14cbbbdebf72156b7e21081bd2b61578d82c0feb
ysmreg <33682244+ysmreg@users.noreply.github.com>
2022-08-07 20:56:33 +0900

    fix: 修正

--- routes/help.js
+++ routes/help.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const ws = require('./ws');
 const sleep = require('sleep');
 lpack = [];
 lpack["ja"] = {"catdesc": "このページはSakuraBotのコマンドヘルプです。カテゴリを選択することにより、カテゴリに含まれるコマンドを見ることが出来ます","cmddesc":"このページはSakuraBotのコマンドヘルプです。コマンドを選択するとコマンドの説明を表示します。もしグループコマンドの場合はグループ内のコマンドリストを表示します"};
 lpack["en"] = {"catdesc": "This page is the command help for SakuraBot. By selecting a category, you can see the commands included in that category.", "cmddesc": "This page is the command help for SakuraBot. By selecting a command, you can see a description of the command. If it is a group command, a list of commands in the group will be displayed"};
 router.get('/', async function (req, res) {
+    
     const { l,cat,cmd } = req.query;
     if (!l){
         ll = "ja";
