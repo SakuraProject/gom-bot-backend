20eaf1ae5d3846be43996a3881cb47dc7b80573d
ysmreg <33682244+ysmreg@users.noreply.github.com>
2023-03-05 12:59:05 +0900

    fix: 落ちないようにした

--- index.js
+++ index.js
@@ -38,4 +38,7 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
     }
 );
+process.on("uncaughtException", (e) => console.log(e));
 module.exports = {};
+module.exports.app = app;
+
